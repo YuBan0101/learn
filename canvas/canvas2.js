62616a47a70626dd98976d22a580aa5bd14c88ce
hasaki <ssxxvvc@qq.com>
2018-06-12 20:37:55 +0800

    study2

--- canvas/canvas2.js
+++ canvas/canvas2.js
@@ -8,3 +8,15 @@ var image = document.images[0];
 
 ctx.drawImage(image,10,10);
 
+ctx.strokeStyle = "#0000ff";
+ctx.fillStyle = "rgba(45,45,45,0.2)";
+ctx.moveTo(100,100);
+ctx.lineTo(200,100);
+//ctx.stroke();
+ctx.lineTo(150,165);
+ctx.lineTo(100,100);
+ctx.moveTo(100,135);
+ctx.lineTo(200,135);
+ctx.lineTo(150,65);
+ctx.lineTo(100,135);
+ctx.stroke();
